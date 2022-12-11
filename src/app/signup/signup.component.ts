8c688ff55d15e2744449a8cdfe6eedb2c81eb07a
“Your <lamichhanejenita@gmail.com>
2022-12-11 12:59:34 +0545

    Added built in validation for form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  signupForm!: FormGroup 
+  submitted = false
+  loading =false
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formSignup();
+  }
+  formSignup(){
+    this.signupForm =this.formBuilder.group({
+      username:['', Validators.required],
+      password:['', Validators.required],
+      email:['', [Validators.required,Validators.email]],
+      number:['', Validators.required]
+    })
+  }
+  get f() {
+    return this.signupForm.controls;
   }
 
+  onSubmit(){
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    this.loading = true
+    
+    console.log(this.signupForm.value);
+    this.signupForm.reset();
+
+    if(this.signupForm.value){
+      alert('Thank you for signup.Your signup process is successfull');
+    }
+    else{
+      alert('Not successfull')
+    }
+    
+  }
+
+
+  
 }
